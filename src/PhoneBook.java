b30a0571eb31295c8a61f1f7feb7447d5007132c
Irina Nasyrova <irina.a.nasyrova@gmail.com>
2020-09-28 01:14:37 +0000

    add action selection, add init

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,7 +1,42 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
-        //Добавить считывание ввода пользователя в цикле
+        String[][] book = new String[3][2];
+        init(book);
+
+        Scanner scanner = new Scanner(System.in);
+        int value = 0;
+        while (value != 3) {
+            System.out.println("Введите цифру (1 - добавить в справочник, 2 - печать справочника, 3 - выход): ");
+            value = scanner.nextInt();
+            switch (value) {
+                case 1:
+                    break;
+                case 2:
+                    list(book);
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Неверный символ! Пожалуйста, повторите ввод.");
+            }
+        }
+
+    }
+
+    private static void init(String[][] book) {
+        book[0][0] = "Сергеева Мария Анатольевна";
+        book[0][1] = "8 888 454 33 33";
+
+        book[1][0] = "Иванов Иван Иванович";
+        book[1][1] = "8 911 999 99 99";
+
+        book[2][0] = "Яшкин Валентин Петрович";
+        book[2][1] = "8 788 567 44 44";
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
@@ -21,7 +56,7 @@ public class PhoneBook {
     }
 
     public static void add(String[][] book, String name, String number) {
-        //add logic
+
     }
 
     public static void list(String[][] book) {
